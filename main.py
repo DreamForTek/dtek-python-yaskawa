2134efe34918113598dad797ab8cb0bffc138881
rui <ruisebastiao@gmail.com>
2021-12-21 11:35:07 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+#
+# Copyright (C) 2019, 2021 DFit
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Authors:
+#    Rui Sebastião <rui.sebastiao@dreamforit.com>
+#
+
+
+import socketio
+
+import eventlet
+
+sio = socketio.Server()
+
+app = socketio.WSGIApp(sio, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def my_message(sid, data):
+    print('message ', data)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+
